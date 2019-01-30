4d6136a678b3c17fb6bb4d444e87c115bf76e45a
T2 <studioTeaTwo@gmail.com>
2019-01-30 18:07:57 +0900

    feat: inquiry

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <About/>
+      <Inquiry/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from "./components/About";
+import Inquiry from "./components/Inquiry";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    About,
+    Inquiry
+  },
+  data() {
+    return {
+      //
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  background: #eceff1;
+  color: rgba(0, 0, 0, 0.87);
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+@media (max-width: 600px) {
+  body,
+  #about,
+  #inquiry {
+    margin-top: 0;
+    background: white;
+    box-shadow: none;
+  }
+  /* body { border-top: 16px solid #ffa100; } */
+}
+
+h1 {
+  font-size: 22px;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 0.6);
+  margin: 0 0 16px;
+}
+
+h2 {
+  color: #ffa100;
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0 0 8px;
+}
+
+p {
+  line-height: 140%;
+  margin: 16px 0 24px;
+  font-size: 14px;
 }
 </style>
---
+++ src/components/Inquiry.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="inquiry">
+    <v-container>
+      <h2>お問い合わせ</h2>
+      <v-form ref="form" v-model="contactFormValidation.valid" lazy-validation>
+        <v-text-field
+          v-model="contactForm.name"
+          :rules="contactFormValidation.nameRules"
+          label="名前"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="contactForm.email"
+          :rules="contactFormValidation.emailRules"
+          label="メールアドレス"
+          required
+        ></v-text-field>
+        <v-textarea
+          v-model="contactForm.contents"
+          :rules="contactFormValidation.contentsRules"
+          label="内容"
+          required
+        ></v-textarea>
+        <v-btn
+          :loading="contactForm.loading"
+          :disabled="!contactFormValidation.valid"
+          @click="sendMail()"
+          block
+          flat
+          color="info"
+          class="mt-2 font-weight-bold"
+        >送信</v-btn>
+      </v-form>
+    </v-container>
+    <v-snackbar
+      v-model="snackBar.show"
+      :color="snackBar.color"
+      bottom
+      right
+      :timeout="6000"
+      class="font-weight-bold"
+    >{{snackBar.message}}</v-snackbar>
+  </div>
+</template>
+
+<script>
+import { functions } from "@/plugins/firebase";
+
+export default {
+  data: () => ({
+    contactForm: {
+      name: "",
+      contents: "",
+      email: "",
+      loading: false
+    },
+    contactFormValidation: {
+      valid: false,
+      nameRules: [v => !!v || "名前は必須項目です"],
+      emailRules: [v => !!v || "メールアドレスは必須項目です"],
+      contentsRules: [v => !!v || "内容は必須項目です"]
+    },
+    snackBar: {
+      show: false,
+      color: "",
+      message: ""
+    }
+  }),
+  methods: {
+    sendMail: function() {
+      if (this.$refs.form.validate()) {
+        this.contactForm.loading = true;
+        const mailer = functions.httpsCallable("sendMail");
+
+        mailer(this.contactForm)
+          .then(() => {
+            this.formReset();
+            this.showSnackBar(
+              "success",
+              "お問い合わせありがとうございます。送信完了しました"
+            );
+          })
+          .catch(err => {
+            this.showSnackBar(
+              "error",
+              "送信に失敗しました。時間をおいて再度お試しください"
+            );
+            console.log(err);
+          })
+          .finally(() => {
+            this.contactForm.loading = false;
+          });
+      }
+    },
+    showSnackBar: function(color, message) {
+      this.snackBar.message = message;
+      this.snackBar.color = color;
+      this.snackBar.show = true;
+    },
+    formReset: function() {
+      this.$refs.form.reset();
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+#inquiry {
+  background: white;
+  max-width: 480px;
+  margin: 20px auto 16px;
+  padding: 32px 24px;
+  border-radius: 3px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+</style>
