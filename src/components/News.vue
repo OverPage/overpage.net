79177ffc3e3e783e0ba11cce8a28d2d9818d953c
T2 <studioTeaTwo@gmail.com>
2019-09-23 16:56:24 +0900

    doc: add news

--- src/components/News.vue
+++ src/components/News.vue
@@ -3,6 +3,9 @@
     <v-container>
       <h2>プレスリリース</h2>
       <a
+        href="https://prtimes.jp/main/html/rd/p/000000003.000041982.html"
+      >2019/9/22 ブロックチェーンを活用したYouTuber/VTuber向けマーケティング支援ツール「アポイタカラ 」をメインネットに移行</a>
+      <a
         href="https://prtimes.jp/main/html/rd/p/000000002.000041982.html"
       >2019/4/30 ブロックチェーンを活用したYouTuber/VTuber向けマーケティング支援ツール「アポイタカラ 」で音声ファイルのトークンを開始</a>
       <a
